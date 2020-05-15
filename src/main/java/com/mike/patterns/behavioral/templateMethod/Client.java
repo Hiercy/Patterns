c21143b2e326b0f2860dce22c3fa01931612e774
Бешлык Михаил <beshlyk.mikhail@gmail.com>
2020-05-16 00:05:44 +0300

    Delete useless imports

--- src/main/java/com/mike/patterns/behavioral/templateMethod/Client.java
+++ src/main/java/com/mike/patterns/behavioral/templateMethod/Client.java
@@ -1,8 +1,5 @@
 package com.mike.patterns.behavioral.templateMethod;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Client {
