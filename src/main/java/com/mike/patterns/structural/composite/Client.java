4c877466b670743351accd7592e7b970b5fc0860
Hiercy <beshlyk.mikhail@gmail.com>
2020-05-28 10:22:26 +0300

    /*/

--- src/main/java/com/mike/patterns/structural/composite/Client.java
+++ src/main/java/com/mike/patterns/structural/composite/Client.java
@@ -3,12 +3,15 @@ package com.mike.patterns.structural.composite;
 public class Client {
     public static void main(String[] args) {
         LetterComposite starWars = new Messenger().messageFromStarWars();
+        System.out.println(starWars.count());
         starWars.print();
 
         LetterComposite schwarz = new Messenger().messageFromSchwarzenegger();
+        System.out.println(schwarz.count());
         schwarz.print();
 
         LetterComposite joker = new Messenger().messageFromJoker();
+        System.out.println(joker.count());
         joker.print();
     }
 }
